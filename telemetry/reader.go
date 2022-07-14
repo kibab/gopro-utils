ea8e305621acad24b05b33376f62c8d10f65ae53
Max8Curtis <52663084+Max8Curtis@users.noreply.github.com>
2022-07-14 16:37:36 +0100

    Added STMP label

--- telemetry/reader.go
+++ telemetry/reader.go
@@ -56,6 +56,7 @@ func Read(f io.Reader) (*TELEM, error) {
                 "HUES",
                 "SROT",
                 "TIMO",
+		"STMP",
                 "GPSA",
                 "CORI",
                 "IORI",
